4fae9be5bcf475da42bb4b8f21332220fb566dda
Bryan Davis <bd808@bd808.com>
2023-11-30 15:06:50 -0700

    mute-senders: add notefromgithub and logmsgbot_cloud

--- scripts.js
+++ scripts.js
@@ -250,7 +250,7 @@
   var MUTED,
     indexOf = [].indexOf;
 
-  MUTED = ['analytics-logbot', 'antispammeta', 'arseny', 'asimovbot', 'cerebrumbot', 'cp', 'cyberpower', 'danny_b', 'ecmabot-wm', 'eir', 'gh-datavalues', 'gh-wmde', 'github', 'github-wmde', 'grrrit-wm', 'guest', 'icinga-wm', 'ircnotifier', 'jinxer-wm', 'jinxer-wm-test', 'justberry', 'krrrit-wm', 'labs-morebots', 'libel', 'librenms-wmf', 'mw-jenkinsbot', 'nokib', 'opendevreview', 'paladox', 'phabot', 'phawikibugs', 'phawkes', 'pywikibugs', 'services_bot', 'shinken-wm', 'sirenbot', 'slander', 'snitch', 'stashbot', 'stewardbot', 'travis-ci', 'wikibugs', 'wikimediagithub', 'wikipedia-github', 'wikiphabot', 'wm-bot', 'wm-labs-meetbot', 'wmf-insecte', 'wmf-selenium-bot', 'wmopbot', 'zppix'];
+  MUTED = ['analytics-logbot', 'antispammeta', 'arseny', 'asimovbot', 'cerebrumbot', 'cp', 'cyberpower', 'danny_b', 'ecmabot-wm', 'eir', 'gh-datavalues', 'gh-wmde', 'github', 'github-wmde', 'grrrit-wm', 'guest', 'icinga-wm', 'ircnotifier', 'jinxer-wm', 'jinxer-wm-test', 'justberry', 'krrrit-wm', 'labs-morebots', 'libel', 'librenms-wmf', 'logmsgbot_cloud', 'mw-jenkinsbot', 'nokib', 'notefromgithub', 'opendevreview', 'paladox', 'phabot', 'phawikibugs', 'phawkes', 'pywikibugs', 'services_bot', 'shinken-wm', 'sirenbot', 'slander', 'snitch', 'stashbot', 'stewardbot', 'travis-ci', 'wikibugs', 'wikimediagithub', 'wikipedia-github', 'wikiphabot', 'wm-bot', 'wm-labs-meetbot', 'wmf-insecte', 'wmf-selenium-bot', 'wmopbot', 'zppix'];
 
   Textual.bind('messageAddedToView', function(line) {
     var cleanNick, e, ref, type;
